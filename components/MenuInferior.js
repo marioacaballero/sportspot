8a85a72b23a46b3c7c9fa41c955c9a59d782265b
Mauricio <madegasperis@gmail.com>
2024-01-25 10:57:55 -0300

    fix: cambio de rama

--- components/MenuInferior.js
+++ components/MenuInferior.js
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 import { StyleSheet, View, Pressable, Image } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { Color } from '../GlobalStyles'
