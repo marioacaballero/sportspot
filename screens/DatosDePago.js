b95232cf928cd7486eb7c137bcbf778a91854a07
CristianPerez <norberto.cp@hotmail.com>
2024-01-29 09:33:47 -0300

    fix: correccion de vistas y boton de regreso

--- screens/DatosDePago.js
+++ screens/DatosDePago.js
@@ -2,13 +2,50 @@ import * as React from 'react'
 import { StyleSheet, View, Text, Pressable, Image } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { Color, FontFamily, Padding, FontSize, Border } from '../GlobalStyles'
+import { Path, Rect, Svg } from 'react-native-svg'
 
 const DatosDePago = () => {
   const navigation = useNavigation()
 
   return (
     <View style={styles.datosDePago}>
-      <View style={[styles.pencillineParent, styles.pencillineParentPosition]}>
+      <View
+        style={{
+          // paddingTop: 30,
+          // paddingHorizontal: 15,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          width: '100%'
+        }}
+      >
+        <Text style={[styles.gestionaTuCuentaContainer, styles.labelFlexBox]}>
+          GESTIONA TU CUENTA
+        </Text>
+        <Pressable onPress={() => navigation.goBack()}>
+          <Svg width="25" height="25" viewBox="0 0 21 21" fill="none">
+            <Rect
+              width="21"
+              height="21"
+              transform="translate(0 21) rotate(-90)"
+              fill="white"
+            />
+            <Path
+              d="M6.17798 4.98006L0.65625 10.5018L6.17798 16.0234L7.10604 15.0953L3.16862 11.158L20.3124 11.158L20.3124 9.84546L3.16874 9.84546L7.10604 5.90816L6.17798 4.98006Z"
+              fill={Color.sportsVioleta}
+            />
+          </Svg>
+        </Pressable>
+      </View>
+      <View
+        style={{
+          width: '100%',
+          marginTop: 15
+        }}
+      >
+        <Text style={styles.datosDePago2}>Datos de pago</Text>
+      </View>
+      <View style={styles.pencillineParent}>
         <View style={styles.pencilline}>
           <Image
             style={styles.walletIcon}
@@ -22,45 +59,25 @@ const DatosDePago = () => {
               Datos de pago
             </Text>
           </View>
-          <Text
-            style={[styles.imGoingTo, styles.imGoingToFlexBox]}
-          >{`Añade o elimina métdos de pago de forma segura para agilizar el proceso de inscripción
-
-`}</Text>
-        </View>
-      </View>
-      <View style={styles.gestionaTuCuentaWrapper}>
-        <Text
-          style={[styles.gestionaTuCuentaContainer, styles.imGoingToFlexBox]}
-        >
-          {'GESTIONA TU '}CUENTA
-        </Text>
-      </View>
-      <View style={[styles.datosDePagoInner, styles.pencillineParentPosition]}>
-        <View style={styles.datosDePagoContainer}>
-          <Text style={[styles.datosDePago2, styles.datosTypo]}>
-            Datos de pago
+          <Text style={[styles.imGoingTo, styles.imGoingToFlexBox]}>
+            Añade o elimina métdos de pago de forma segura para agilizar el
+            proceso de inscripción
           </Text>
+          <Pressable
+            style={styles.aadirTarjetaWrapper}
+            onPress={() => navigation.navigate('Metodo')}
+          >
+            <Text style={[styles.aadirTarjeta, styles.datosTypo]}>
+              Añadir tarjeta
+            </Text>
+          </Pressable>
         </View>
       </View>
-      <Pressable
-        style={styles.aadirTarjetaWrapper}
-        onPress={() => navigation.navigate('Metodo')}
-      >
-        <Text style={[styles.aadirTarjeta, styles.datosTypo]}>
-          Añadir tarjeta
-        </Text>
-      </Pressable>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  pencillineParentPosition: {
-    width: 320,
-    // left: 20
-    position: 'absolute'
-  },
   imGoingToFlexBox: {
     color: Color.sportsVioleta,
     textAlign: 'left',
@@ -110,14 +127,17 @@ const styles = StyleSheet.create({
     textAlign: 'left'
   },
   frameParent: {
-    width: 201,
-    height: 83,
+    width: 330,
+
     paddingHorizontal: Padding.p_3xs,
     paddingBottom: Padding.p_3xs,
     paddingTop: Padding.p_mini
   },
   pencillineParent: {
-    top: 172,
+    // position: 'realtive',
+    padding: 20,
+    width: '100%',
+    top: 30,
     borderRadius: Border.br_3xs,
     shadowColor: 'rgba(0, 0, 0, 0.25)',
     shadowOffset: {
@@ -130,19 +150,20 @@ const styles = StyleSheet.create({
     borderStyle: 'solid',
     borderColor: Color.colorGainsboro_100,
     borderWidth: 1,
-    height: 148,
-    flexDirection: 'row'
+    // height: 148,
+    flexDirection: 'row',
+    alignSelf: 'center',
+    justifyContent: 'center'
   },
   gestionaTuCuentaContainer: {
     fontSize: FontSize.size_5xl,
-    textAlign: 'left',
     fontFamily: FontFamily.inputPlaceholder,
-    fontWeight: '700'
+    fontWeight: '700',
+    alignSelf: 'stretch'
   },
-  gestionaTuCuentaWrapper: {
-    top: 67,
-    left: 20,
-    position: 'absolute'
+  labelFlexBox: {
+    textAlign: 'left',
+    color: Color.sportsVioleta
   },
   datosDePago2: {
     color: Color.sportsNaranja,
@@ -154,28 +175,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row'
   },
-  datosDePagoInner: {
-    top: 140,
-    justifyContent: 'center'
-  },
+
   aadirTarjeta: {
     fontSize: FontSize.inputPlaceholder_size,
     color: Color.blanco,
     textAlign: 'left'
   },
   aadirTarjetaWrapper: {
-    top: 257,
-    // left: 40,
+    marginTop: 50,
     borderRadius: Border.br_31xl,
     backgroundColor: Color.sportsVioleta,
-    width: 281,
-    height: 43,
+    width: '100%',
     paddingHorizontal: 0,
     paddingVertical: Padding.p_6xs,
-    justifyContent: 'center',
     alignItems: 'center',
-    flexDirection: 'row',
-    position: 'absolute'
+    right: 30
   },
   icon: {
     height: '100%',
@@ -213,13 +227,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row'
   },
+
   datosDePago: {
     backgroundColor: Color.blanco,
-    height: 800,
+    // height: 800,
     // overflow: 'hidden',
+    paddingTop: 30,
+    paddingHorizontal: 15,
     width: '100%',
     flex: 1,
-    justifyContent: 'center',
+    // justifyContent: 'center',
     alignItems: 'center'
   }
 })
