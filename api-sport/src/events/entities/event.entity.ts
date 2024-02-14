e26c321b8e5a1d9fe7a51ca07e2d4ceafdb7c855
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-02-14 11:34:20 -0300

    Update event.entity.ts

--- api-sport/src/events/entities/event.entity.ts
+++ api-sport/src/events/entities/event.entity.ts
@@ -1,5 +1,29 @@
-import { Entity } from 'typeorm'
+import { Entity, Column, NumericType } from 'typeorm'
 import { BaseEntity } from 'src/config/base.entity'
 
 @Entity({ name: 'event' })
-export class Event extends BaseEntity {}
+export class EventEntity extends BaseEntity {
+  @Column()
+  title: string
+
+  @Column()
+  description: string
+
+  @Column()
+  sport: string
+
+  @Column('decimal')
+  price: NumericType
+
+  @Column()
+  modality: string
+
+  @Column()
+  location: string
+
+  @Column()
+  dateStart: Date
+
+  @Column()
+  dateInscription: Date
+}
