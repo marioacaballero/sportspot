1bb821224d3cd98fcd409e34cf4d42fac57a11f0
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-02-14 12:49:00 -0300

    user notification relation

--- api-sport/src/notifications/notifications.module.ts
+++ api-sport/src/notifications/notifications.module.ts
@@ -3,9 +3,10 @@ import { NotificationsService } from './notifications.service'
 import { NotificationsController } from './notifications.controller'
 import { NotificationEntity } from './entities/notification.entity'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { UserEntity } from 'src/users/entities/users.entity'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([NotificationEntity])],
+  imports: [TypeOrmModule.forFeature([NotificationEntity, UserEntity])],
   controllers: [NotificationsController],
   providers: [NotificationsService]
 })
