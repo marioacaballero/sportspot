d8e050feda857ce52831f5ca4fe23b219ce7303b
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-02-14 11:34:11 -0300

    Update create-event.dto.ts

--- api-sport/src/events/dto/create-event.dto.ts
+++ api-sport/src/events/dto/create-event.dto.ts
@@ -1,1 +1,39 @@
-export class CreateEventDto {}
+import { Type } from 'class-transformer'
+import { IsDate, IsDecimal, IsNotEmpty, IsString } from 'class-validator'
+import { NumericType } from 'typeorm'
+
+export class CreateEventDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string
+
+  @IsNotEmpty()
+  @IsString()
+  description: string
+
+  @IsNotEmpty()
+  @IsString()
+  sport: string
+
+  @IsNotEmpty()
+  @IsDecimal()
+  price: NumericType // Usando IsDecimal para asegurar que el precio puede incluir decimales
+
+  @IsNotEmpty()
+  @IsString()
+  modality: string
+
+  @IsNotEmpty()
+  @IsString()
+  location: string
+
+  @IsNotEmpty()
+  @Type(() => Date)
+  @IsDate()
+  dateStart: Date
+
+  @IsNotEmpty()
+  @Type(() => Date)
+  @IsDate()
+  dateInscription: Date
+}
