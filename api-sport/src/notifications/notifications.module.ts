ebc483c15e862202691ec9d2aa83b86d30f575a4
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-02-14 11:34:00 -0300

    Update notifications.module.ts

--- api-sport/src/notifications/notifications.module.ts
+++ api-sport/src/notifications/notifications.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { NotificationsService } from './notifications.service';
-import { NotificationsController } from './notifications.controller';
+import { Module } from '@nestjs/common'
+import { NotificationsService } from './notifications.service'
+import { NotificationsController } from './notifications.controller'
+import { NotificationEntity } from './entities/notification.entity'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([NotificationEntity])],
   controllers: [NotificationsController],
-  providers: [NotificationsService],
+  providers: [NotificationsService]
 })
 export class NotificationsModule {}
