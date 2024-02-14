bc3b7cd92b5344e89cbae02da21cec3ce19748dd
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-02-14 11:34:08 -0300

    Update create-notification.dto.ts

--- api-sport/src/notifications/dto/create-notification.dto.ts
+++ api-sport/src/notifications/dto/create-notification.dto.ts
@@ -1,1 +1,31 @@
-export class CreateNotificationDto {}
+import { IsBoolean, IsNotEmpty, IsString, IsDate } from 'class-validator'
+
+export class CreateNotificationDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string
+
+  @IsNotEmpty()
+  @IsString()
+  message: string
+
+  @IsNotEmpty()
+  @IsDate()
+  date: Date
+
+  @IsNotEmpty()
+  @IsString()
+  eventType: string
+
+  @IsNotEmpty()
+  @IsString()
+  eventId: string
+
+  @IsNotEmpty()
+  @IsString()
+  recipientId: string
+
+  @IsNotEmpty()
+  @IsBoolean()
+  read: boolean = false
+}
