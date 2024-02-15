f940e6730b6c10cca98cb189b6e4914b3be9dbea
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-02-15 14:56:12 -0300

    Update users.service.ts

--- api-sport/src/users/services/users.service.ts
+++ api-sport/src/users/services/users.service.ts
@@ -3,12 +3,17 @@ import { UserEntity } from '../entities/users.entity'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { UserDTO } from '../dto/user.dto'
+import { EventEntity } from 'src/events/entities/event.entity'
+import { UpdateUserDto } from '../dto/update-user.dto'
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(UserEntity)
-    private readonly userRepository: Repository<UserEntity>
+    private readonly userRepository: Repository<UserEntity>,
+
+    @InjectRepository(EventEntity)
+    private readonly eventRepository: Repository<EventEntity>
   ) {}
 
   public async createService(body: UserDTO) {
@@ -22,6 +27,7 @@ export class UsersService {
     return await this.userRepository
       .createQueryBuilder('user')
       .where({ id })
+      .leftJoinAndSelect('user.events', 'events')
       .getOne()
   }
 
@@ -32,21 +38,49 @@ export class UsersService {
 
   public async updateService(
     id: string,
-    newName: string,
-    newEmail: string
+    updateUserDto: UpdateUserDto
   ): Promise<UserEntity> {
-    const user = await this.userRepository
-      .createQueryBuilder('user')
-      .where({ id })
+    const user = await this.getOneService(id)
+
+    const event = await this.eventRepository
+      .createQueryBuilder('event')
+      .where({ id: updateUserDto.eventId })
       .getOne()
 
     if (!user) {
       throw new Error(`Usuario con ID ${id} no encontrado`)
     }
 
-    user.name = newName
-    user.email = newEmail
+    user.name = updateUserDto.name
+    user.email = updateUserDto.email
+
+    if (event && !user.events.some((e) => e.id === event.id))
+      user.events = [...user.events, event]
 
     return await this.userRepository.save(user)
+  }
+
+  public async deleteSubscriptionService(
+    userId: string,
+    eventId: string
+  ): Promise<UserEntity> {
+    const user = await this.getOneService(userId)
+    if (!user) {
+      throw new Error(`Usuario con ID ${userId} no encontrado`)
+    }
+    console.log(eventId)
+    const event = await this.eventRepository
+      .createQueryBuilder('event')
+      .where({ id: eventId })
+      .getOne()
+
+    // Cargar la relación de eventos para poder modificarla
+    await this.userRepository
+      .createQueryBuilder()
+      .relation(UserEntity, 'events')
+      .of(user)
+      .remove(event)
+
+    return await this.getOneService(userId) // Recargar el usuario para reflejar los cambios
   }
 }
