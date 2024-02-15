1859585dff33ad23c05e5aed2abacfd3caa44709
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-02-15 10:10:07 -0300

    Update users.entity.ts

--- api-sport/src/users/entities/users.entity.ts
+++ api-sport/src/users/entities/users.entity.ts
@@ -1,7 +1,8 @@
 // usuario.entity.ts
 import { BaseEntity } from 'src/config/base.entity'
+import { EventEntity } from 'src/events/entities/event.entity'
 import { NotificationEntity } from 'src/notifications/entities/notification.entity'
-import { Entity, Column, OneToMany } from 'typeorm'
+import { Entity, Column, OneToMany, ManyToMany } from 'typeorm'
 
 @Entity({ name: 'user' })
 export class UserEntity extends BaseEntity {
@@ -16,4 +17,12 @@ export class UserEntity extends BaseEntity {
 
   @OneToMany(() => NotificationEntity, (notification) => notification.recipient)
   notifications: NotificationEntity[]
+
+  // Relación de uno a muchos con los eventos creados
+
+  @OneToMany(() => EventEntity, (event) => event.creator)
+  eventsCreated: EventEntity[]
+
+  @ManyToMany(() => EventEntity, (event) => event.suscribers)
+  events: EventEntity[]
 }
