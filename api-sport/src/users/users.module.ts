ae719719c1e838a1c7771bdc362c41a74acec271
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-02-16 10:26:10 -0300

    Update users.module.ts

--- api-sport/src/users/users.module.ts
+++ api-sport/src/users/users.module.ts
@@ -4,11 +4,15 @@ import { UsersController } from './controllers/users.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { UserEntity } from './entities/users.entity'
 import { EventEntity } from 'src/events/entities/event.entity'
+import { NotificationEntity } from 'src/notifications/entities/notification.entity'
+import { NotificationsService } from 'src/notifications/notifications.service'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity, EventEntity])],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity, EventEntity, NotificationEntity])
+  ],
   exports: [] /* [que quiero exportar] */,
-  providers: [UsersService],
+  providers: [UsersService, NotificationsService],
   controllers: [UsersController]
 })
 export class UsersModule {}
