17cc78611805f304cfa2e79b3f131b9350d16f76
Mauricio <madegasperis@gmail.com>
2024-02-22 13:58:58 -0300

    fix: template de mail de registro

--- api-sport/src/send-mails/send-mails.service.ts
+++ api-sport/src/send-mails/send-mails.service.ts
@@ -7,20 +7,130 @@ export class SendMailsService {
   constructor(private readonly mailerService: MailerService) {}
 
   async sendRegistrationNotification(email: string) {
+    const sportspotLogo = join(
+      __dirname,
+      '..',
+      '..',
+      '..',
+      'client',
+      'assets',
+      'spotsport.png'
+    )
+    const facebookIcon = join(
+      __dirname,
+      '..',
+      '..',
+      '..',
+      'client',
+      'assets',
+      'icons',
+      'facebook_icon.png'
+    )
+    const twitterIcon = join(
+      __dirname,
+      '..',
+      '..',
+      '..',
+      'client',
+      'assets',
+      'icons',
+      'twitter_icon.png'
+    )
+    const instagramIcon = join(
+      __dirname,
+      '..',
+      '..',
+      '..',
+      'client',
+      'assets',
+      'icons',
+      'instagram_icon.png'
+    )
     const htmlTemplate = `
+    <html>
+    <head>
       <style>
+        body {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          height: 150vh;
+          margin: 0;      
+        }
+        #container {
+          text-align: center;
+          padding: 20px;
+          background-color: #fcece7;
+        }
+        img {
+          width: 40%;
+          height: auto;
+          display: block;
+          margin: 0 auto;
+        }
         p {
-          color: blue;
+          color: #642794;
+          text-align: center;,       
+        }
+        .title {
+          font-size: 2em;
+          font-weight: bold;
+        }
+        .social {
+          font-weight: 600;
+          font-size: 1.5em;
+        }
+        .icons {
+          display: flex;
+          flex-direction: row;
+          width: 40%;
+          justify-content: center;
+          align-items: center;
+          margin-left: 30%;
+        }
+        .iconImg {
+          width: 40px;
         }
       </style>
-      <p>¡Gracias por registrarte!</p>
-      <p>Este es un correo de prueba.</p>
+    </head>
+    <body>
+    <div id="container">
+    <img src="cid:sportSpot" />
+        <p class='title'>¡Gracias por registrarte!</p>
+        <p>Ya estás listo para comenzar a participar en los mejores eventos deportivos en el área que desees</p>
+          <p class='social'>¡Síguenos en nuestras redes!</p>
+          <div class='icons'>
+            <img src="cid:facebookIcon" class='iconImg'/>
+            <img src="cid:twitterIcon" class='iconImg'/>
+            <img src="cid:instagramIcon" class='iconImg'/>
+          </div>
+      </div>
+    </body>
+    </html>
     `
     await this.mailerService.sendMail({
       to: email,
       subject: 'Registro exitoso',
       html: htmlTemplate, // Archivo de plantilla de correo electrónico
-      context: {} // Datos adicionales que pueden ser pasados a la plantilla
+      context: {}, // Datos adicionales que pueden ser pasados a la plantilla
+      attachments: [
+        {
+          filename: 'sportspot.png',
+          path: sportspotLogo,
+          cid: 'sportSpot'
+        },
+        {
+          filename: 'facebook_icon.png',
+          path: facebookIcon,
+          cid: 'facebookIcon'
+        },
+        { filename: 'twitter_icon.png', path: twitterIcon, cid: 'twitterIcon' },
+        {
+          filename: 'instagram_icon.png',
+          path: instagramIcon,
+          cid: 'instagramIcon'
+        }
+      ]
     })
     console.log(join(__dirname, '..', 'template', 'registration'))
     return 'Correo enviado exitosamente'
