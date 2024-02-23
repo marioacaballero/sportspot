4ae5e625b453ec074f368ccfbabfa380e22b7755
CristianPerez <norberto.cp@hotmail.com>
2024-02-23 13:37:09 -0300

    imagenes ok desde celular

--- api-sport/src/main.ts
+++ api-sport/src/main.ts
@@ -4,12 +4,16 @@ import { AppModule } from './app.module'
 import * as morgan from 'morgan'
 import { ValidationPipe } from '@nestjs/common'
 import { CORS } from './config/cors'
+import { json, urlencoded } from 'express'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.use(morgan('dev'))
   app.enableCors(CORS)
+
+  app.use(json({ limit: '100mb' }))
+  app.use(urlencoded({ limit: '100mb', extended: true }))
 
   app.useGlobalPipes(
     new ValidationPipe({
