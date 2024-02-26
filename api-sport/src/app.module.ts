dc753edd5f72226e5231f0febceced8cdc90811a
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-02-26 11:56:40 -0300

    Update app.module.ts

--- api-sport/src/app.module.ts
+++ api-sport/src/app.module.ts
@@ -10,9 +10,13 @@ import { JsonwebtokenModule } from './jsonwebtoken/jsonwebtoken.module'
 import { SendMailsModule } from './send-mails/send-mails.module'
 
 import { join } from 'path'
+import { PassportModule } from '@nestjs/passport'
+
+import { GoogleStrategyModule } from './google-strategy/google-strategy.module'
 console.log(join(__dirname, 'template'))
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'google' }),
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     TypeOrmModule.forRoot(DataSourceConfig),
     UsersModule,
@@ -20,7 +24,8 @@ console.log(join(__dirname, 'template'))
     NotificationsModule,
     SportsModule,
     JsonwebtokenModule,
-    SendMailsModule
+    SendMailsModule,
+    GoogleStrategyModule
   ],
   controllers: [],
   providers: []
