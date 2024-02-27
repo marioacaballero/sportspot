ec4cb462a816e246713fd167957abd3c8ab8f4d1
Maximinliano Permingeat <38232063+mpermingeat@users.noreply.github.com>
2024-02-27 09:48:12 -0300

    Delete google-strategy.module.ts

--- api-sport/src/google-strategy/google-strategy.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GoogleStrategyService } from './google-strategy.service';
-import { GoogleStrategyController } from './google-strategy.controller';
-
-@Module({
-  controllers: [GoogleStrategyController],
-  providers: [GoogleStrategyService],
-})
-export class GoogleStrategyModule {}
