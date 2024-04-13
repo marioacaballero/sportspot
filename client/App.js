3d9c3621173fc3f69a00d7e6d840000ba60b9f50
MarcosX <79601596+Marco5X@users.noreply.github.com>
2024-04-13 10:53:09 -0300

    correccion en App y readme en back

--- client/App.js
+++ client/App.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { NavigationContainer } from '@react-navigation/native'
+import {
+  NavigationContainer,
+  useNavigationState
+} from '@react-navigation/native'
 import { loadFonts } from './GlobalStyles'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { Provider } from 'react-redux'
@@ -44,6 +47,183 @@ import { StatusBar } from 'react-native'
 // import { StripeProvider } from '@stripe/stripe-react-native'
 // import PaymentScreen from './screens/PaymentScreen'
 
+function MyStackNavigator({ isFooterShow, setIsFooterShow }) {
+  const state = useNavigationState((state) => state) // Obtiene el estado actual de la navegación
+
+  useEffect(() => {
+    if (state) {
+      // Comprueba si state existe antes de intentar acceder a state.routes
+      const currentRouteName = state.routes[state.index].name // Obtiene el nombre de la ruta actual
+
+      // Actualiza el estado isFooterShow basado en la ruta actual
+      setIsFooterShow(
+        currentRouteName !== 'Bienvenida' &&
+          currentRouteName !== 'IniciarSesin' &&
+          currentRouteName !== 'SignIn' &&
+          currentRouteName !== 'Registrarse' &&
+          currentRouteName !== 'RecuperarContraseña'
+      )
+    }
+  }, [state])
+
+  return (
+    <Stack.Navigator
+      initialRouteName="Bienvenida"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen
+        name="PruebasEncontradasDetalle"
+        component={PruebasEncontradasDetalle}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="VentajasSuscripciones"
+        component={VentajasSuscripciones}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="stripe"
+        component={StripeComponent}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="EditarPerfil"
+        component={EditarPerfil}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="PublicarEvento"
+        component={PublicarEvento}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Directorio"
+        component={Directorio}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Seguridad"
+        component={Seguridad}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Metodo"
+        component={Metodo}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="DatosDePago"
+        component={DatosDePago}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Cuenta"
+        component={Cuenta}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Metodo1"
+        component={Metodo1}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Bienvenida"
+        component={Bienvenida}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="SignIn"
+        component={SignIn}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="EscribirResea"
+        component={EscribirResea}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Favoritos"
+        component={Favoritos}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="UltimasConsultas"
+        component={UltimasConsultas}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Favoritos1"
+        component={Favoritos1}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="HistorialDePruebas"
+        component={HistorialDePruebas}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="TuPerfil"
+        component={TuPerfil}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="IniciarSesin"
+        component={IniciarSesin}
+        options={{ headerShown: false, title: 'Atrás' }}
+      />
+      <Stack.Screen
+        name="Registrarse"
+        component={Registrarse}
+        options={{ headerShown: false, title: 'Atrás' }}
+      />
+      <Stack.Screen
+        name="InicioDeportista"
+        component={InicioDeportista}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Organizador"
+        component={InicioOrganizador}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="PruebasEncontradas"
+        component={PruebasEncontradas}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="InicioBUSCADOR"
+        component={InicioBUSCADOR}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Popupfiltros"
+        component={Popupfiltros}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="InicioSUSCRIPCIONES"
+        component={InicioSUSCRIPCIONES}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="PopupAlerta"
+        component={PopupAlerta}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Contacta"
+        component={Contacta}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="RecuperarContraseña"
+        component={RecuperarContraseña}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  )
+}
 const Stack = createNativeStackNavigator()
 
 export default function App() {
@@ -64,177 +244,15 @@ export default function App() {
         backgroundColor="#fff"
       />
       <Provider store={store}>
-        {/* <StripeProvider publishableKey={KEY}> */}
-        <NavigationContainer>
-          <StripeProvider publishableKey={KEY}>
-            {/* <StripeProvider publishableKey="pk_test_51OpBGEEOUHrYS47YxO9vPqjLLRu9F387GV3jMcc4rEtayJX9n7hKCyXw0cGlJCk7foCP89YpQUT4gD02XoGVK50l005VwrZaGC"> */}
-            <Stack.Navigator
-              initialRouteName="Bienvenida"
-              screenOptions={({ route }) => ({
-                headerShown: false,
-                footerShown: setIsFooterShow(
-                  route.name !== 'Bienvenida' &&
-                    route.name !== 'IniciarSesin' &&
-                    route.name !== 'SignIn' &&
-                    route.name !== 'Registrarse' &&
-                    route.name !== 'RecuperarContraseña'
-                )
-              })}
-            >
-              <Stack.Screen
-                name="PruebasEncontradasDetalle"
-                component={PruebasEncontradasDetalle}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="VentajasSuscripciones"
-                component={VentajasSuscripciones}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="stripe"
-                component={StripeComponent}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="EditarPerfil"
-                component={EditarPerfil}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="PublicarEvento"
-                component={PublicarEvento}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="Directorio"
-                component={Directorio}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="Seguridad"
-                component={Seguridad}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="Metodo"
-                component={Metodo}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="DatosDePago"
-                component={DatosDePago}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="Cuenta"
-                component={Cuenta}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="Metodo1"
-                component={Metodo1}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="Bienvenida"
-                component={Bienvenida}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="SignIn"
-                component={SignIn}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="EscribirResea"
-                component={EscribirResea}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="Favoritos"
-                component={Favoritos}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="UltimasConsultas"
-                component={UltimasConsultas}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="Favoritos1"
-                component={Favoritos1}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="HistorialDePruebas"
-                component={HistorialDePruebas}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="TuPerfil"
-                component={TuPerfil}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="IniciarSesin"
-                component={IniciarSesin}
-                options={{ headerShown: false, title: 'Atrás' }}
-              />
-              <Stack.Screen
-                name="Registrarse"
-                component={Registrarse}
-                options={{ headerShown: false, title: 'Atrás' }}
-              />
-              <Stack.Screen
-                name="InicioDeportista"
-                component={InicioDeportista}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="Organizador"
-                component={InicioOrganizador}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="PruebasEncontradas"
-                component={PruebasEncontradas}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="InicioBUSCADOR"
-                component={InicioBUSCADOR}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="Popupfiltros"
-                component={Popupfiltros}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="InicioSUSCRIPCIONES"
-                component={InicioSUSCRIPCIONES}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="PopupAlerta"
-                component={PopupAlerta}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="Contacta"
-                component={Contacta}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="RecuperarContraseña"
-                component={RecuperarContraseña}
-                options={{ headerShown: false }}
-              />
-            </Stack.Navigator>
+        <StripeProvider publishableKey={KEY}>
+          <NavigationContainer>
+            <MyStackNavigator
+              isFooterShow={isFooterShow}
+              setIsFooterShow={setIsFooterShow}
+            />
             {isFooterShow && <MenuInferior />}
-          </StripeProvider>
-        </NavigationContainer>
+          </NavigationContainer>
+        </StripeProvider>
       </Provider>
     </SafeAreaView>
   )
--- client/utils/apiBackend.js
+++ client/utils/apiBackend.js
@@ -3,7 +3,9 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
   // baseURL: 'http://212.47.234.94:3000/'
-  baseURL: 'https://api-sportspot.ay-cloud.com/api/'
+  // baseURL: 'https://api-sportspot.ay-cloud.com/api/'
+  // baseURL: 'http://181.209.120.141:3000/'
+  baseURL: 'http://192.168.1.179:3000/api'
   // baseURL: 'http://192.168.18.82:3000'
   // maxContentLength: Infinity,
   // maxBodyLength: Infinity
