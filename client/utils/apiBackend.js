be5d063eeb98c4de9cdf8c9df234f002b6f15297
Ferk94 <fernando.kaganovicz@gmail.com>
2024-04-15 19:39:12 -0300

    fix: fix apibackend url

--- client/utils/apiBackend.js
+++ client/utils/apiBackend.js
@@ -5,7 +5,7 @@ const axiosInstance = axios.create({
   // baseURL: 'http://212.47.234.94:3000/'
   // baseURL: 'https://api-sportspot.ay-cloud.com/api/'
   // baseURL: 'http://181.209.120.141:3000/'
-  baseURL: 'http://192.168.0.144:3000/api'
+  baseURL: 'http://c5e7a731-cc55-4137-a1e8-04915e54dee9.pub.instances.scw.cloud:3000/api'
   // baseURL: 'http://192.168.18.82:3000'
   // maxContentLength: Infinity,
   // maxBodyLength: Infinity
