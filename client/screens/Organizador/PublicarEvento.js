8e5ca866e88866bbfcf55334db01786d7a64d5f3
Alejo Ufano <ufanoalejo@gmail.com>
2024-05-24 17:33:38 -0300

    feat: event edit, etc

--- client/screens/Organizador/PublicarEvento.js
+++ client/screens/Organizador/PublicarEvento.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, StyleSheet, Text, Pressable, ScrollView } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 import { Color, FontFamily, FontSize } from '../../GlobalStyles'
 import BackArrowSVG from '../../components/SVG/BackArrowSVG'
 import FormularioEventos from '../../components/FomularioEventos'
@@ -8,6 +8,21 @@ import { LinearGradient } from 'expo-linear-gradient'
 
 const PublicarEvento = () => {
   const navigation = useNavigation()
+  const route = useRoute()
+  const params = route.params
+  const [eventData, setEventData] = useState()
+  const [onEditMode, setOnEditMode] = useState()
+
+  useEffect(() => {
+    if (params && params?.onEdit) {
+      console.log('event from params:', params?.eventState)
+      console.log('onEdit from params: ', params?.onEdit)
+      setEventData(params?.eventState)
+      setOnEditMode(true)
+    }
+  }, [])
+
+  console.log('onEditMode: ', onEditMode)
 
   return (
     <LinearGradient
@@ -25,7 +40,27 @@ const PublicarEvento = () => {
             </Pressable>
           </View>
           <View style={styles.innerContainer}>
-            <FormularioEventos />
+            <FormularioEventos
+              onEditMode={onEditMode}
+              eventData={params?.eventState}
+              formattedEventData={{
+                title: params?.eventState?.title || '',
+                description: params?.eventState?.description || '',
+                price: params?.eventState?.price || '',
+                dateStart: params?.eventState?.dateStart || '',
+                dateEnd: params?.eventState?.dateInscription || '',
+                location: params?.eventState?.location || '',
+                timeStart: params?.eventState?.timeStart || '',
+                sportId: params?.eventState?.sportId || '',
+                eventLink: params?.eventState?.eventLink || '',
+                inscriptionLink: params?.eventState?.inscriptionLink || '',
+                places: params?.eventState?.places?.toString() || '',
+                mail: params?.eventState?.creator?.email || '',
+                image: params?.eventState?.image || '',
+                phoneNumber: params?.eventState?.phoneNumber?.toString() || ''
+              }}
+              setEventData={setEventData}
+            />
           </View>
         </View>
       </ScrollView>
