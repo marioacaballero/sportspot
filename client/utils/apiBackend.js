aaee010c053999ef337a4a7a10885f40a63bcd04
azul <azschiaffino@gmail.com>
2024-07-10 23:12:15 -0300

    fix

--- client/utils/apiBackend.js
+++ client/utils/apiBackend.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://192.168.0.77:3000/api'
-  // baseURL:
-  //   'http://c5e7a731-cc55-4137-a1e8-04915e54dee9.pub.instances.scw.cloud:3000/api'
+  // baseURL: 'http://192.168.100.2:3000/api'
+  baseURL:
+    'http://c5e7a731-cc55-4137-a1e8-04915e54dee9.pub.instances.scw.cloud:3000/api'
   // API_IP ||
 })
 
